1183cefefb002320dda5449dbefa110927d18077
Tom Cook <github@tom.horse>
2018-08-11 20:14:55 -0700

    Brew setup script
    Basic list of commands to install all of the applications needed on a clean macOS system, installed via `brew cask` and some other brew cli utils.

---
+++ macos_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Packages installed via Brew
+#
+
+# Install brew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install gpg htop nmap wget jq
+
+# all systems
+
+brew cask install google-chrome-beta 1password 1password-cli spotify iterm2 atom github evernote homebrew/cask-versions/slack-beta keybase caffeine istat-menus
+
+# personal only
+
+brew cask install battlenet discord tunnelbear steam
+
+# extra setup steps
+
+open /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
