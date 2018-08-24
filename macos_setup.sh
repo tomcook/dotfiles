0ec2f523147b93781d6339f450db44c72f1ba497
Tom Cook <github@tom.horse>
2018-08-23 23:48:41 -0700

    Add vscode and transmit to brew install list

--- macos_setup.sh
+++ macos_setup.sh
@@ -11,7 +11,7 @@ brew install gpg htop nmap wget jq httpie
 
 # all systems
 
-brew cask install google-chrome-beta 1password 1password-cli spotify iterm2 atom github evernote homebrew/cask-versions/slack-beta keybase caffeine istat-menus
+brew cask install google-chrome-beta 1password 1password-cli spotify iterm2 atom visual-studio-code github evernote homebrew/cask-versions/slack-beta keybase caffeine istat-menus transmit
 
 # personal only
 
