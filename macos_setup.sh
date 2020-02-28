145d36dd56948006ffd28ff7b083a49914fe0605
Tom Cook <tomcook@users.noreply.github.com>
2020-02-27 21:27:46 -0800

    Add mas tool, and install critical Mac App Store apps with it

--- macos_setup.sh
+++ macos_setup.sh
@@ -7,7 +7,13 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install gpg htop nmap wget jq httpie
+brew install gpg htop nmap wget jq httpie mas
+
+# Mac App Store
+#
+# (In order below) XCode, Bear, LINE, Keynote, Soulver, Pixelmator, WireGuard
+
+mas install 497799835 1091189122 539883307 409183694 413965349 407963104 1451685025
 
 # all systems
 
