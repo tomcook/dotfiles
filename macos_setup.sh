37ed2ca2e64aae9797048437563ee907bc5cf2b1
Tom Cook <tomcook@users.noreply.github.com>
2021-01-02 14:34:06 -0800

    New way to fetch homebrew for install
    They no longer use ruby directly.

--- macos_setup.sh
+++ macos_setup.sh
@@ -5,7 +5,7 @@
 
 # Install brew
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install gpg htop nmap wget jq httpie mas
 
