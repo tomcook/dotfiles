34da46cea3c73c03abe65704af6028beb9e52270
Tom Cook <819843+tomcook@users.noreply.github.com>
2022-07-09 16:25:08 -0700

    Updating software lists
    brew, mas, and removing battlenet setup

--- macos_setup.sh
+++ macos_setup.sh
@@ -7,7 +7,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install gpg htop nmap wget jq httpie mas
+brew install gpg htop nmap wget jq httpie mas bat dog exa tailscale
 
 # Mac App Store
 
@@ -17,16 +17,12 @@ mas install 497799835 1464122853 937984704 1451685025
 
 # Personal Stuff
 # Bear, Brother Scanner Software, Day One, LINE, Keynote, Soulver, Pixelmator
-mas install 1091189122 1193539993 1055511498 539883307 409183694 413965349 407963104
+mas install 1091189122 1193539993 1055511498 539883307 409183694 1508732804 407963104
 
 # all systems
 
-brew install --cask docker homebrew/cask-versions/google-chrome-beta little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus transmit hashicorp/tap/terraform hashicorp/tap/packer
+brew install --cask docker homebrew/cask-versions/google-chrome-beta docker little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus transmit hashicorp/tap/terraform hashicorp/tap/packer
 
 # personal only
 
-brew cask install battle-net discord steam
-
-# extra setup steps
-
-open /usr/local/Caskroom/battle-net/latest/Battle.net-Setup.app
+brew install --cask battle-net discord steam
