c20e1ca7a552053fa411bca44d1c99a6464fec6e
Tom Cook <819843+tomcook@users.noreply.github.com>
2022-07-09 16:46:56 -0700

    Change Tailscale install method
    Also hashicorp apps

--- macos_setup.sh
+++ macos_setup.sh
@@ -10,7 +10,9 @@ chsh -s /bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install gpg htop nmap wget jq httpie mas bat dog exa tailscale
+brew tap hashicorp/tap
+
+brew install gpg htop nmap wget jq httpie mas bat dog exa
 
 # Mac App Store
 
@@ -24,8 +26,19 @@ mas install 1091189122 1193539993 1055511498 539883307 409183694 1508732804 4079
 
 # all systems
 
-brew install --cask docker homebrew/cask-versions/google-chrome-beta docker little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus transmit hashicorp/tap/terraform hashicorp/tap/packer
+brew install --cask docker homebrew/cask-versions/google-chrome-beta docker little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus transmit 
+
+# Hashicorp
+
+brew install hashicorp/tap/packer hashicorp/tap/terraform
 
 # personal only
 
 brew install --cask battle-net discord steam
+
+# Tailscale
+
+cd /tmp
+wget https://pkgs.tailscale.com/stable/Tailscale-1.26.2-macos.zip
+unzip Tailscale-1.26.2-macos.zip
+mv Tailscale.app /Applications/
