f48e18cba6fd772582e4dc3cb70b1366a397f2fb
Tom Cook <819843+tomcook@users.noreply.github.com>
2022-07-09 17:27:53 -0700

    Some system settings and stream deck

--- macos_setup.sh
+++ macos_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-#
-# Packages installed via Brew
-#
 
 # Change default shell to bash
 chsh -s /bin/bash
+
+# Install Rosetta 2
+/usr/sbin/softwareupdate –install-rosetta –agree-to-license
+
+# Remove everything from the Dock
+defaults write com.apple.dock persistent-apps -array
+killall Dock
 
 # Install brew
 
@@ -13,6 +17,18 @@ chsh -s /bin/bash
 brew tap hashicorp/tap
 
 brew install gpg htop nmap wget jq httpie mas bat dog exa
+
+# all systems
+
+brew install --cask docker homebrew/cask-versions/google-chrome-beta docker maestral little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus soundsource transmit 
+
+# Hashicorp
+
+brew install hashicorp/tap/packer hashicorp/tap/terraform
+
+# personal only
+
+brew install --cask battle-net discord steam
 
 # Mac App Store
 
@@ -24,21 +40,14 @@ mas install 497799835 1464122853 937984704 1451685025
 # Bear, Brother Scanner Software, Day One, LINE, Keynote, Soulver, Pixelmator
 mas install 1091189122 1193539993 1055511498 539883307 409183694 1508732804 407963104
 
-# all systems
-
-brew install --cask docker homebrew/cask-versions/google-chrome-beta docker little-snitch 1password 1password-cli spotify iterm2 aerial visual-studio-code github homebrew/cask-versions/slack-beta istat-menus soundsource transmit 
-
-# Hashicorp
-
-brew install hashicorp/tap/packer hashicorp/tap/terraform
-
-# personal only
-
-brew install --cask battle-net discord steam
-
 # Tailscale
 
 cd /tmp
 wget https://pkgs.tailscale.com/stable/Tailscale-1.26.2-macos.zip
 unzip Tailscale-1.26.2-macos.zip
 mv Tailscale.app /Applications/
+
+# Stream Deck
+cd /tmp
+wget https://edge.elgato.com/egc/macos/sd/Stream_Deck_5.3.1.15197.pkg
+open Stream_Deck_5.3.1.15197.pkg
